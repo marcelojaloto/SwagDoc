6dc4711a547e380f5bd1ce7a0429f881d67bdaed
geoffsmith82 <github@tysontechnology.com.au>
2018-04-24 09:57:10 +1000

    Added handling of response headers

--- Demos/LoadSwaggerJsonToObject/SwagDocDemo.dpr
+++ Demos/LoadSwaggerJsonToObject/SwagDocDemo.dpr
@@ -15,6 +15,7 @@ uses
   Swag.Doc.Path in '..\..\Source\Swag.Doc.Path.pas',
   Swag.Doc.SecurityDefinition in '..\..\Source\Swag.Doc.SecurityDefinition.pas',
   Swag.Doc.SecurityDefinitionApiKey in '..\..\Source\Swag.Doc.SecurityDefinitionApiKey.pas',
+  Swag.Doc.Path.Operation.ResponseHeaders in '..\..\Source\Swag.Doc.Path.Operation.ResponseHeaders.pas',
   Swag.Doc.Info.License in '..\..\Source\Swag.Doc.Info.License.pas';
 
 {$R *.res}
