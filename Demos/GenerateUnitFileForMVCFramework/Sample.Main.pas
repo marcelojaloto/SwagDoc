d96a4ac3b92e988d2058a85855881ad571341fc5
7th-marcelo-jaloto <marcelo.jaloto@seventh.com.br>
2019-08-07 15:00:54 -0300

    Add license;

--- Demos/GenerateUnitFileForMVCFramework/Sample.Main.pas
+++ Demos/GenerateUnitFileForMVCFramework/Sample.Main.pas
@@ -4,6 +4,8 @@
 {  Copyright (c) 2018 Marcelo Jaloto                                           }
 {  https://github.com/marcelojaloto/SwagDoc                                    }
 {                                                                              }
+{  Sample author: geoffsmith82 - 2019                                          }
+{                                                                              }
 {******************************************************************************}
 {                                                                              }
 {  Licensed under the Apache License, Version 2.0 (the "License");             }
